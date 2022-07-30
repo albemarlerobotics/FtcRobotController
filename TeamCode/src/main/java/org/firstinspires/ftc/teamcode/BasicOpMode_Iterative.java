60baee9c0225313b266cfdc715e98683abc31dc5
Emmett Lach <emmett.lach@gmail.com>
2022-07-30 01:27:55 -0400

    Setup motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOpMode_Iterative.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOpMode_Iterative.java
@@ -1,8 +1,47 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
+
+@TeleOp(name="Basic: Iterative OpMode", group="Iterative Opmode")
+public class BasicOpMode_Iterative extends OpMode {
+    private DcMotor frontLeftMotor = null;
+    private DcMotor frontRightMotor = null;
+    private DcMotor backLeftMotor = null;
+    private DcMotor backRightMotor = null;
+
+    @Override
+    public void init() {
+        frontLeftMotor  = hardwareMap.get(DcMotor.class, "front_left_motor");
+        frontRightMotor = hardwareMap.get(DcMotor.class, "front_right_motor");
+        backLeftMotor  = hardwareMap.get(DcMotor.class, "back_left_motor");
+        backRightMotor = hardwareMap.get(DcMotor.class, "back_right_motor");
+
+        frontLeftMotor.setDirection(DcMotor.Direction.FORWARD);
+        frontRightMotor.setDirection(DcMotor.Direction.REVERSE);
+        backLeftMotor.setDirection(DcMotor.Direction.FORWARD);
+        backRightMotor.setDirection(DcMotor.Direction.REVERSE);
+
+        frontLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontRightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backRightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        frontLeftMotor.setPower(0);
+        frontRightMotor.setPower(0);
+        backLeftMotor.setPower(0);
+        backRightMotor.setPower(0);
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
